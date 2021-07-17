7bdb05029a91f577b276458916c57a27646c6ee7
DAKSH900 <70480346+DAKSH900@users.noreply.github.com>
2021-07-17 10:43:30 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,34 @@
+#This my project that callculates triangle missing side by pythagous theoram
+#i have imported time to put gap between the sentences
+import time 
+#i have imported math to calculate squar root
+import math
+print("Hi")
+time.sleep(1)
+print("I will calculate triangle missing side lenght for u")
+time.sleep(1)
+base = int(input("pls tell base lenght of triangle after equal to sign === "))
+time.sleep(1)
+height = int(input("pls tell hight lenght of triangle after equal to sign === "))
+print("okay calculating")
+print("..............................")
+time.sleep(1)
+print("..............................")
+time.sleep(1)
+print("..............................")
+time.sleep(1)
+print("..............................")
+time.sleep(1)
+print("..............................")
+print(base*base + height*height )
+answer = int(input("pls tell ans lenght of triangle after equal to sign === "))
+num_sqrt = math.sqrt(answer)
+time.sleep(1)
+print("YOUR ANSWER IS IN PROCESS PLS WAIT")
+time.sleep(1)
+print(f'{answer} your ans is {num_sqrt}')
+
+print("THANKU FOR USING ME")
+time.sleep(10)
+#YOU COPY THE WHOLE PROGRAM....
+#CREDIT FOR THIS PROJECT GOES TO DAKSH THAKUR
